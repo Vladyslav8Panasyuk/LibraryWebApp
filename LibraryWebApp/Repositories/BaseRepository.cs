22f69189ac1cb95c044d8f8eb4a5886782306b5b
Vladyslav Panasyuk <panasyuk.vladyslav.kpi@gmail.com>
2025-05-07 00:51:30 +0300

    feat: add base repository

---
+++ LibraryWebApp/Repositories/BaseRepository.cs
@@ -0,0 +1,47 @@
+﻿using LibraryWebApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryWebApp.Repositories
+{
+    public class BaseRepository<T> : IRepository<T> where T : class
+    {
+        protected readonly ApplicationDbContext _context;
+
+        public BaseRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T?> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task RemoveAsync(int id)
+        {
+            var entity = await GetByIdAsync(id);
+            if (entity != null)
+            {
+                _context.Set<T>().Remove(entity);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
