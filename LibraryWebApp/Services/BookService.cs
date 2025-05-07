821382aff76502d56b73489124bd18c08d41e6c3
Vladyslav Panasyuk <panasyuk.vladyslav.kpi@gmail.com>
2025-05-07 17:44:12 +0300

    feat: add book service

---
+++ LibraryWebApp/Services/BookService.cs
@@ -0,0 +1,53 @@
+﻿using LibraryWebApp.Models;
+using LibraryWebApp.Repositories;
+
+namespace LibraryWebApp.Services
+{
+    public class BookService : IBookService
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public BookService(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        public async Task<IEnumerable<Book>> GetBooksByLibraryAsync(int libraryId)
+        {
+            return await _bookRepository.GetAllByLibraryAsync(libraryId);
+        }
+
+        public async Task<Book?> GetBookByIdAsync(int id)
+        {
+            return await _bookRepository.GetByIdAsync(id);
+        }
+
+        public async Task CreateBookAsync(Book book)
+        {
+            await _bookRepository.AddAsync(book);
+            await _bookRepository.SaveChangesAsync();
+        }
+
+        public async Task UpdateBookAsync(Book book)
+        {
+            var existingBook = await _bookRepository.GetByIdAsync(book.Id);
+            if (existingBook == null)
+            {
+                throw new KeyNotFoundException($"Book with ID {book.Id} not found.");
+            }
+            _bookRepository.Update(book);
+            await _bookRepository.SaveChangesAsync();
+        }
+
+        public async Task DeleteBookAsync(int id)
+        {
+            var book = await _bookRepository.GetByIdAsync(id);
+            if (book == null)
+            {
+                throw new KeyNotFoundException($"Book with ID {id} not found.");
+            }
+            _bookRepository.Remove(book);
+            await _bookRepository.SaveChangesAsync();
+        }
+    }
+}
