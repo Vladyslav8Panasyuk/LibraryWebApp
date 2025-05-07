255d90524b940ab58d6c9f9ee2f414f72e651a8c
Vladyslav Panasyuk <panasyuk.vladyslav.kpi@gmail.com>
2025-05-07 16:10:12 +0300

    feat: add reader repository

---
+++ LibraryWebApp/Repositories/IReaderRepository.cs
@@ -0,0 +1,9 @@
+﻿using LibraryWebApp.Models;
+
+namespace LibraryWebApp.Repositories
+{
+    public interface IReaderRepository : IRepository<Reader>
+    {
+        Task<IEnumerable<Reader>> GetAllByLibraryAsync(int libraryId);
+    }
+}
