036f45112627fb8cd5d99c83993c94d201cd4cf5
Vladyslav Panasyuk <panasyuk.vladyslav.kpi@gmail.com>
2025-05-07 16:32:58 +0300

    feat: add library repository

---
+++ LibraryWebApp/Repositories/ILibraryRepository.cs
@@ -0,0 +1,9 @@
+﻿using LibraryWebApp.Models;
+
+namespace LibraryWebApp.Repositories
+{
+    public interface ILibraryRepository : IRepository<Library>
+    {
+        Task<Library?> GetByIdWithBooksAndReadersAsync(int id);
+    }
+}
---
+++ LibraryWebApp/Repositories/LibraryRepository.cs
@@ -0,0 +1,19 @@
+﻿using LibraryWebApp.Data;
+using LibraryWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryWebApp.Repositories
+{
+    public class LibraryRepository : BaseRepository<Library>, ILibraryRepository
+    {
+        public LibraryRepository(ApplicationDbContext context) : base(context) { }
+        
+        public async Task<Library?> GetByIdWithBooksAndReadersAsync(int id)
+        {
+            return await _context.Libraries
+                .Include(library => library.Books)
+                .Include(library => library.Readers)
+                .FirstOrDefaultAsync(library => library.Id == id);
+        }
+    }
+}
